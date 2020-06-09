73311b4bce2e7eae473f725092d0c4c25936f674
bruno <bruno.1301817@fapi-pinhais.edu.br>
2020-06-08 23:13:22 -0300

    Biblioteca xlrd

--- Projeto/teste-bruno.py
+++ Projeto/teste-bruno.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import xlrd
 
-dados = pd.read_excel('../Dados/0NMfcDpYXo_2018-05-31_2019-05-31.xlsx')
-print('ioi')
+dados = pd.read_excel('Dados/0NMfcDpYXo_2018-05-31_2019-05-31.xlsx')
+print(dados.head())
