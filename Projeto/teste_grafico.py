08b5d76d384a4510fbd09480783d28db4bd44f98
bruno <bruno.1301817@fapi-pinhais.edu.br>
2020-06-09 23:47:21 -0300

    teste grafico

--- Projeto/teste_grafico.py
+++ Projeto/teste_grafico.py
@@ -4,8 +4,25 @@ import os
 import matplotlib.pyplot
 
 
+dados = pd.read_excel('Dados/0NMfcDpYXo_2018-05-31_2019-05-31.xlsx')
+
+matplotlib.pyplot
+#matplotlib.pyplot.scatter(dados['likes'], dados['views'], dados['likes'])
+#dados['likes'].plot.hist(bins=30, edgecolor='black', title="Número de likes" ) # barra horizontal
+#dados['likes'].value_counts().plot.bar()      #barras vertica
+#dados['likes'].value_counts().plot.barh()     #barras Horiental
+#dados['date'].value_counts().plot.barh(title="Número de likes")   #barras Horiental com titulos
+#dados.plot.scatter(x='views', y='likes', s=.5)
+#dados.sample(frac=.1).plot.scatter(x='views', y='likes')   #grafico de area
+#dados.plot.hist(bins=30, edgecolor='black')      #grafico de area
+#matplotlib.pyplot.plot(dados.likes, dados.date)
+
+matplotlib.pyplot.show()
+
+'''
 meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho']
 valores = [105235, 107697, 110256, 109236, 108859, 109986]
 matplotlib.pyplot.plot(meses, valores)
 
 matplotlib.pyplot.show()
+'''
---
+++ Projeto/teste_grafico2.py
@@ -0,0 +1,4 @@
+import pandas as pd
+import plotly.offline as py
+import plotly.graph_objs as go
+#py.init_notebook_mode(connected=True)
