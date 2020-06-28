e780e50baafc6ffe39cc87f23c699e939761c090
bruno <bruno.1301817@fapi-pinhais.edu.br>
2020-06-28 00:23:49 -0300

    ...+

--- Projeto/TemplateGrafico3BarGraphObjs.py
+++ Projeto/TemplateGrafico3BarGraphObjs.py
@@ -6,7 +6,7 @@ import pandas as pd
 import plotly.offline as py
 import plotly.graph_objs as go
 
-fig = go.Figure(data=go.Bar(x = ['Banana', 'Maçã', 'Uva'],
+fig = go.Figure(data=go.Bar(x = ['=', 'Maçã', 'Uva'],
                 y = [10, 20, 30],
                 name = 'Gráfico 1',
                 marker = {'color': '#feca57'}))
---
+++ Projeto/mediaSemanalDeViews.py
@@ -0,0 +1,60 @@
+#Faz a media de todos os dias de cada mes e monta um grafico
+import pandas as pd
+import xlrd
+import os
+import plotly.express as px
+
+pd.set_option('display.max_columns', 30)
+pd.set_option('display.width', 20000)
+pd.set_option('display.max_rows', 50000)
+
+source = "Dados"
+
+dir_list = os.listdir(source)
+
+os.chdir(source)
+dadosMaster = pd.DataFrame([])
+for i in range(len(dir_list)):
+    filename = dir_list[i]
+    dados = pd.read_excel(filename)
+    dadosMaster = dadosMaster.append(dados)
+
+dadosMaster['diasSemana'] = pd.to_datetime(dadosMaster['date']).dt.day_name()
+
+output = dadosMaster[['diasSemana', 'views']].groupby('diasSemana').mean()
+output.reset_index(inplace=True)
+print("Construção da informaçaõ finalizada")
+print("Iniciando construção do grafico")
+df = output
+px.defaults.color_continuous_scale = px.colors.sequential.Blackbody
+fig = px.bar(df, x="diasSemana", y="views", color="diasSemana",
+
+             title="Media Semanal de views 2018 - 2019")
+
+fig.update_layout(
+    title={
+        # 'text': "Media mensal de views 2018 - 2019",
+        'y': 0.9,
+        'x': 0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'},
+
+    xaxis_title="Dia Da Semana",
+    yaxis_title="Quantidade de views",
+    legend_title_text='Dias Da Semana',
+    xaxis=dict(
+
+       type='category'
+    ),
+
+    font=dict(
+        family="Courier New, monospace",
+        size=18,
+        color="#297554"
+
+    )
+)
+
+fig.write_html('../Output/Media_Semanal.html', auto_open=True)
+print("Gravação do grafico finalizada")
+fig.show()
